ffee6e3bdf60e258d884794e5f013c5495aa82e8
neolani16 <116310184+neolani16@users.noreply.github.com>
2022-12-19 11:42:18 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+function hello(x) {
+    alert("hello " + x);
+}
+
+function secretmessage(message) {
+
+    console.log("The secret message is" + message);
+}
+
+
+hello("can you find the secret message?")
+//fix the call to secretemessage
+// function secretMessage(message)
+secretmessage("ddkdd");
+
